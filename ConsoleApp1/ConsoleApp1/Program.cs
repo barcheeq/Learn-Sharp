4c3c54c8cfc55fb89312125a5a2ae1c93de89864
Artem Butenko <rott.corpse@gmail.com>
2022-10-26 17:50:13 +0300

    Homework

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace ConsoleApp1
+﻿using System.Text.Json.Nodes;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            int number1 = 0;
+            bool number2 = false;
+            float number3 = 0.999f;
+            long number4 = 1234567890;
+            string text = "Hello";
+
+
+
+
+
         }
     }
 }
